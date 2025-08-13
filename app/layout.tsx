9abce34482e071818a3a17541d3cda9f8d7f5253
Kevin Irungu <kirungu481@gmail.com>
2025-08-13 12:01:10 +0300

    Updated footer import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import MainTitle from "@/components/MainTitle";
 import { Toaster } from "react-hot-toast";
 
@@ -67,7 +67,7 @@ export default function RootLayout({
         <Header />
         <MainTitle />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
