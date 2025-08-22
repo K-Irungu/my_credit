cf345d95ebdbcfbc6cc0d6ebf8888a4cbe06cf70
Kevin Irungu <kirungu481@gmail.com>
2025-08-22 14:14:46 +0300

    Updated logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,17 +1,29 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from "winston";
+
+// Check environment
+const isProd = process.env.NODE_ENV === "production";
 
 const logger = createLogger({
-    level: process.env.LOG_LEVEL || 'info',
-    format: format.combine(
-        format.timestamp(),
-        format.errors({ stack: true }),
-        format.json()
-    ),
-    transports: [
-        new transports.Console(),
-        new transports.File({ filename: 'logs/app.log', maxsize: 5242880, maxFiles: 5 })
-    ],
-    exitOnError: false,
+  level: process.env.LOG_LEVEL || "info",
+  format: format.combine(
+    format.timestamp(),
+    format.errors({ stack: true }),
+    format.json()
+  ),
+  transports: [
+    new transports.Console(), 
+    ...(isProd
+      ? [] 
+      : [
+          new transports.File({
+            filename: "logs/app.log",
+            maxsize: 5 * 1024 * 1024, // 5MB
+            maxFiles: 5,
+          }),
+        ]),
+  ],
+  exitOnError: false,
 });
+
 
 export default logger;
