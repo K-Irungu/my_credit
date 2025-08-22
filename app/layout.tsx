96671714c53d70b4ac57db00ca234a34f1fd0dbb
Kevin Irungu <kirungu481@gmail.com>
2025-08-22 12:28:12 +0300

    Updated Footer import in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import MainTitle from "@/components/MainTitle";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
